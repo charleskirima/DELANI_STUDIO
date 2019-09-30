b8261d89a7baf571055e7744d14bd310485c8f69
charleskinyua <kirimak09@gmail.com>
2019-09-30 07:45:21 +0300

    added toogle js

--- delani.js
+++ delani.js
@@ -13,7 +13,12 @@ $(document).ready(function(){
   }
 
 
-  function myFunction() {
-    var element = document.getElementById("myDIV");
-    element.classList.toggle("mystyle");
- }
+ $(".img1").click(function() {
+  $(".p1").toggle();
+});
+$(".img2").click(function() {
+    $(".p2").toggle();
+});
+$(".img3").click(function() {
+    $(".p3").toggle();
+});
